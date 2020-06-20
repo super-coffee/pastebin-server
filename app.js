346adcad1e9955041756376636c5a5a25f3a49df
Xiao_Jin <742905606@qq.com>
2020-06-20 15:36:53 +0800

    update error handler

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var apiRouter = require('./routes/api');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,19 +23,22 @@ app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // res.status(err.status || 500);
+  // res.render('error');
+
+  // Send error
+  res.send({ status: err.status || 500 })
 });
 
 module.exports = app;
